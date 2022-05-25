5b7c756f62ac48811bd062d53f0a32218d27a95f
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-25 14:18:20 +0530

    Modify quicklinks order and fix stylng of cards

--- components/QuickLinks.vue
+++ components/QuickLinks.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="quicklinks-cards">
-    <div v-for="quickLink in quickLinks" :key="quickLink.name">
-      <a :href="quickLink.link" target="_blank">
-        <v-card class="quicklinks-card">
-          <v-stack
-            direction="column"
-            sm-direction="column"
-            align="start"
-            gap="1.25rem"
-            sm-gap="1rem"
-          >
-            <v-text class="quicklinks-title" :weight="700" line-height="1.5">
-              {{ quickLink.title }}
-            </v-text>
-            <v-stack
-              direction="row"
-              align="center"
-              justify="center"
-              gap=".5rem"
-            >
-              <v-text
-                class="quicklinks-link-text"
-                :weight="400"
-                line-height="1.4"
-                color="secondary"
-              >
-                {{ quickLink.linkText }}
-              </v-text>
-              <v-image class="quicklinks-arrow" path="images/arrow-right.svg" />
-            </v-stack>
-          </v-stack>
-        </v-card>
-      </a>
-    </div>
+    <v-card
+      v-for="quickLink in quickLinks"
+      :key="quickLink.name"
+      class="quicklinks-card"
+    >
+      <v-stack
+        direction="column"
+        sm-direction="column"
+        align="start"
+        gap="1.25rem"
+        sm-gap="1rem"
+        justify="space-between"
+        class="quicklinks-content-container"
+      >
+        <v-text class="quicklinks-title" :weight="700" line-height="1.5">
+          {{ quickLink.title }}
+        </v-text>
+        <v-stack direction="row" align="center" justify="center" gap=".5rem">
+          <v-button
+            type="link"
+            class="quicklinks-link-text"
+            :weight="400"
+            line-height="1.4"
+            color="secondary"
+            :label="quickLink.linkText"
+            label-transform="uppercase"
+            :href="quickLink.link"
+            target="_blank"
+          />
+        </v-stack>
+      </v-stack>
+    </v-card>
   </div>
 </template>
 
@@ -44,7 +43,7 @@ export default {
       quickLinks: [
         {
           name: 'docs',
-          title: 'Developer SDK Documentation',
+          title: 'Developer Docs',
           link: 'https://docs.arcana.network',
           linkText: 'View Docs',
         },
@@ -55,21 +54,21 @@ export default {
           linkText: 'View App',
         },
         {
+          name: 'dashboard',
+          title: 'Go to Dashboard',
+          link: 'https://dashboard.arcana.network',
+          linkText: 'Go to Dashboard',
+        },
+        {
           name: 'discord',
-          title: 'Get Support on Integration',
+          title: 'Get Integration Support',
           link: 'https://discord.gg/w6ej4FtqYS',
           linkText: 'Join Discord',
         },
         {
-          name: 'signup',
-          title: 'Get Started with our SDK',
-          link: 'https://dashboard.arcana.network',
-          linkText: 'Sign up',
-        },
-        {
-          name: 'discourse',
+          name: 'report-bugs',
           title: 'Report Bugs',
-          link: 'https://forum.arcana.network',
+          link: '#',
           linkText: 'Submit Bug',
         },
       ],
@@ -87,7 +86,7 @@ a {
 
 .quicklinks-cards {
   display: grid;
-  grid-gap: 1.875rem;
+  grid-gap: 2rem;
   grid-template-columns: repeat(3, 1fr);
   width: 100%;
 
@@ -96,24 +95,19 @@ a {
   }
 
   @media (--viewport-small) {
-    grid-gap: 1.25rem;
+    grid-gap: 1rem;
     grid-template-columns: 1fr;
   }
 }
 
 .quicklinks-card {
-  opacity: 1;
-  transition: opacity 0.15s ease-in;
-
   @media (--viewport-small) {
     padding: 2rem !important;
   }
 }
 
-.quicklinks-card:hover,
-.quicklinks-card:focus {
-  opacity: 0.8;
-  transition: opacity 0.15s ease-in;
+.quicklinks-content-container {
+  height: 100%;
 }
 
 .quicklinks-title {
@@ -125,23 +119,6 @@ a {
     font-size: 1.25rem;
     margin-bottom: 0.5rem;
     max-width: unset;
-  }
-}
-
-.quicklinks-arrow {
-  width: 0.6em;
-}
-
-.btn-submit-bugs {
-  align-self: end;
-  flex-grow: 0;
-  flex-shrink: 0;
-  width: 16rem;
-
-  @media (--viewport-small) {
-    flex-grow: 1;
-    flex-shrink: 1;
-    width: 100%;
   }
 }
 </style>
