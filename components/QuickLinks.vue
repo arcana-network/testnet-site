9e317c586a3828f1f94a537491e2403bcb127c8d
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-06-21 08:28:48 +0530

    Update quicklinks description

--- components/QuickLinks.vue
+++ components/QuickLinks.vue
@@ -11,25 +11,16 @@
         align="start"
         gap="1.25rem"
         sm-gap="1rem"
-        justify="space-between"
         class="quicklinks-content-container"
       >
         <v-text class="quicklinks-title" :weight="700" line-height="1.5">
           {{ quickLink.title }}
         </v-text>
-        <v-stack direction="row" align="center" justify="center" gap=".5rem">
-          <v-button
-            type="link"
-            class="quicklinks-link-text"
-            :weight="400"
-            line-height="1.4"
-            color="secondary"
-            :label="quickLink.linkText"
-            label-transform="uppercase"
-            :href="quickLink.link"
-            target="_blank"
-          />
-        </v-stack>
+        <v-text
+          class="quicklinks-description"
+          line-height="1.5"
+          v-html="quickLink.description"
+        />
       </v-stack>
     </v-card>
   </div>
@@ -43,33 +34,39 @@ export default {
       quickLinks: [
         {
           name: 'docs',
+          title: 'Get SDKs',
+          description:
+            'Use <a href="https://www.npmjs.com/org/arcana" target="_blank">npm</a> or yarn to install Arcana SDKs. Check our <a href="https://github.com/arcana-network" target="_blank">GitHub repositories</a>.',
+        },
+        {
+          name: 'docs',
           title: 'Developer Docs',
-          link: 'https://docs.arcana.network',
-          linkText: 'View Docs',
+          description:
+            'See <a href="https://docs.arcana.network/docs/quick-start/" target="_blank">Arcana Quick Start</a> Guide to begin integrating your dApp with Arcana SDKs. <a href="https://docs.arcana.network/docs/whyan/" target="_blank">Understand Arcana Network</a> concepts and how it works.',
         },
         {
           name: 'demo',
-          title: 'Reference Tutorial App',
-          link: 'https://demo.arcana.network',
-          linkText: 'View App',
+          title: 'Sample dApp',
+          description:
+            'Refer to <a href="https://docs.arcana.network/docs/demo-app/" target="_blank">code samples</a> and <a href="https://github.com/arcana-network/demo-app" target="_blank">tutorials on GitHub</a> on how to integrate and use Arcana SDKs. Check out <a href="https://demo.arcana.network" target="_blank">deployed sample dApp</a>.',
         },
         {
           name: 'dashboard',
-          title: 'Go to Dashboard',
-          link: 'https://dashboard.arcana.network',
-          linkText: 'Go to Dashboard',
+          title: 'Dashboard',
+          description:
+            'Go to the <a href="https://dashboard.arcana.network/" target="_blank">Developer Dashboard</a> and register your dApp, configure storage region, user authentication, and wallet experience settings.',
         },
         {
           name: 'discord',
-          title: 'Get Integration Support',
-          link: 'https://discord.gg/w6ej4FtqYS',
-          linkText: 'Join Discord',
+          title: 'Get Support',
+          description:
+            'Reach out to the Arcana team through our social channels or write to us at <a href="mailto:support@arcana.network">support@arcana.network</a>.',
         },
         {
           name: 'report-bugs',
           title: 'Report Bugs',
-          link: 'https://forum.arcana.network',
-          linkText: 'Submit Bug',
+          description:
+            'Participate in our bug-bounty program. Click here to <a href="https://forum.arcana.network" target="_blank">report bugs</a>.',
         },
       ],
     }
@@ -112,12 +109,19 @@ a {
 
 .quicklinks-title {
   font-size: 1.5rem;
-  margin-bottom: 2rem;
   max-width: 12ch;
 
   @media (--viewport-small) {
     font-size: 1.25rem;
     max-width: unset;
+  }
+}
+
+.quicklinks-description {
+  font-size: 1rem;
+
+  @media (--viewport-small) {
+    font-size: 0.875rem;
   }
 }
 </style>
