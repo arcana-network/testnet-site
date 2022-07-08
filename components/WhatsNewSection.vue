aac599eade7503f9df33f703887b6cbfe66c6d66
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-07-08 13:33:08 +0530

    Update gradient and their position

--- components/WhatsNewSection.vue
+++ components/WhatsNewSection.vue
@@ -1,29 +1,71 @@
 <template>
   <section id="whats-new">
     <v-container>
-      <v-stack align="start" direction="column" sm-direction="column" gap="1.875rem" sm-gap="1.25rem">
-        <v-heading class="features-heading" :level="2" line-height="1.2" :weight="700">
+      <v-stack
+        align="start"
+        direction="column"
+        sm-direction="column"
+        gap="1.875rem"
+        sm-gap="1.25rem"
+      >
+        <v-heading
+          class="features-heading"
+          :level="2"
+          line-height="1.2"
+          :weight="700"
+        >
           What's New
         </v-heading>
       </v-stack>
-      <v-stack justify="space-between" :grow="1" sm-direction="column" class="features-list">
-        <v-blurb v-for="feature in features" :key="`feature-${feature.title}`" :image="feature.icon"
-          :alt="feature.title" :title="feature.title" :description="feature.description" :caption="feature.caption"
-          class="features-blurb" />
+      <v-stack
+        justify="space-between"
+        :grow="1"
+        sm-direction="column"
+        class="features-list"
+      >
+        <v-blurb
+          v-for="feature in features"
+          :key="`feature-${feature.title}`"
+          :image="feature.icon"
+          :alt="feature.title"
+          :title="feature.title"
+          :description="feature.description"
+          :caption="feature.caption"
+          class="features-blurb"
+        />
       </v-stack>
       <div class="release-notes-container">
-        <v-text class="features-description" color="secondary" :weight="400" line-height="1.4">
+        <v-text
+          class="features-description"
+          color="secondary"
+          :weight="400"
+          line-height="1.4"
+        >
           For a complete list of what's new in our beta release, check our
           product
-          <a href="https://docs.beta.arcana.network/docs/rn_beta">release notes</a>.
+          <a href="https://docs.beta.arcana.network/docs/rn_beta"
+            >release notes</a
+          >.
         </v-text>
       </div>
     </v-container>
     <div class="features-illustration">
-      <img src="@/assets/images/features-gradient-top.png" class="gradient-top" />
-      <img src="@/assets/images/features-illustration.png" class="mobile-remove" />
-      <img src="@/assets/images/features-illustration-mobile.png" class="tablet-remove laptop-remove mobile-show" />
-      <img src="@/assets/images/features-gradient-bottom.svg" class="gradient-bottom" />
+      <img
+        src="@/assets/images/section-gradient-top.svg"
+        class="gradient-top"
+      />
+      <img
+        src="@/assets/images/features-illustration.png"
+        class="mobile-remove"
+      />
+      <img
+        src="@/assets/images/features-illustration-mobile.png"
+        class="tablet-remove laptop-remove mobile-show"
+      />
+      <img
+        src="@/assets/images/section-gradient-bottom.svg"
+        class="gradient-bottom"
+      />
     </div>
   </section>
 </template>
@@ -98,7 +140,7 @@ export default {
   }
 }
 
-.features-list>*+* {
+.features-list > * + * {
   @media (--viewport-small) {
     border-top: 1px solid #353535;
   }
@@ -109,20 +151,21 @@ export default {
   margin-top: 2rem;
 
   @media (--viewport-large) {
-    margin-top: 4rem;
-    margin-bottom: -12rem;
+    margin-top: -2rem;
+    margin-bottom: -14rem;
     z-index: -1;
   }
 }
 
 .gradient-top {
-  position: relative;
-  margin-bottom: -7.5%;
+  position: absolute;
+  top: 0;
 }
 
 .gradient-bottom {
-  position: relative;
-  margin-top: -30%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 }
 
 .features-illustration img {
